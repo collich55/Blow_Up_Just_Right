8e8f3bfd1d215ff10ca1e2fd5ca441c2401506e6
Anthony Collichio <collich55@gmail.com>
2021-04-09 20:21:43 -0400

    styling and added show poles event when modal closes

--- src/game_view.js
+++ src/game_view.js
@@ -172,6 +172,44 @@ GameView.prototype.bindKeyHandlers = function bindKeyHandlers() {
     }
   }
 
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  window.onload = function () {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      post[0].show = true;
+      post[1].show = true;
+
+      setTimeout(function () {
+        post[0].show = false;
+        post[1].show = false;
+      }, 1000);
+    }
+  }
+
   function notPressingDown(e) {
     e.preventDefault();
 
